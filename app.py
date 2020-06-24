c025c73762bc553f39305c6d4cabdf40d4b811ba
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-06-24 16:27:48 -0700

    added seed data, included in welcome route and css

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template,  redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, Pet, connect_db
+from models import db, connect_db, Pet
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -12,11 +14,13 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-
 connect_db(app)
 
-db.create_all()
+# db.create_all()
 
 @app.route('/')
 def welcome_page():
-    return render_template('welcome.html')
+    '''Render welcome page and list of current pets'''
+    pets = Pet.query.all()
+    print(pets)
+    return render_template('welcome.html', pets=pets)
--- seed.py
+++ seed.py
@@ -4,11 +4,13 @@ from models import Pet
 db.drop_all()
 db.create_all()
 
+# current pets to add to database
+
 ava = Pet(name="Ava", species="dog", photo_url="https://images.unsplash.com/photo-1543333108-4f3e0f5a7d11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1336&q=80",
           age="3",
           notes="Husky",
           available=True)
-larry = Pet(name="Larry",
+larry = Pet(name="Benny",
             species="cat",
             photo_url="https://images.unsplash.com/photo-1503324280674-50695c3ae35f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
             age="5",
@@ -20,7 +22,6 @@ peaches = Pet(name="Peaches",
               age="3",
               notes="Orange Tabby",
               available=True)
-
 daisy = Pet(name="Daisy",
             species='dog',
             photo_url="https://images.unsplash.com/photo-1562176566-e9afd27531d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
@@ -28,5 +29,5 @@ daisy = Pet(name="Daisy",
             notes="Golden Retriever",
             available=True)
 
-db.session.add([ava, larry, peaches, daisy])
+db.session.add_all([ava, larry, peaches, daisy])
 db.session.commit()
