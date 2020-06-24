827106f3c9396aacd416d071aaf1ec83bffbb8ee
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-06-24 09:59:22 -0700

    starting files

---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+class Pet(db.Model):
+    '''Pet model.'''
+
+    __tablename__ = 'pets'
+
+    default_image_url = 'https://picsum.photos/id/837/367/267/?blur=5'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    species = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text,
+                          nullable=False,
+                          default=default_image_url)
+    age = db.Column(db.Integer, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
+    available = db.Column(db.Boolean, nullable=False, default=True)
